f1dcbc40c6eef0a94d818cc9e8599afc14afe6c9
zhoujie <78351945@qq.com>
2020-02-22 17:30:35 +0800

    修改配置，添加命令读取

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,61 @@
 import * as vscode from "vscode";
 
+const CHANNEL_NAME = "Command On Save";
+const EXTENSION_NAME = "jerrychoux.commandOnSave";
+
 export function activate(context: vscode.ExtensionContext) {
   let extension = new CommandOnSave(context);
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("extension.hello", () => {}),
-    vscode.workspace.onDidSaveTextDocument(e => {
-      let outputChannel: vscode.OutputChannel;
-      outputChannel = vscode.window.createOutputChannel("command on save");
-      outputChannel.show();
-
-      let result = "command on save msg";
-      outputChannel.appendLine(result);
+    vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+      extension.ExecuteCommands(document);
+    }),
+    vscode.workspace.onDidChangeConfiguration(() => {
+      extension.LoadConfig();
     })
   );
 }
 
 export function deactivate() {}
 
+interface ICommand {
+  ext: string;
+  cmd: string;
+}
+
 class CommandOnSave {
+  output: vscode.OutputChannel;
+  context: vscode.ExtensionContext;
+  commands: Array<ICommand>;
+
   constructor(context: vscode.ExtensionContext) {
+    this.output = vscode.window.createOutputChannel(CHANNEL_NAME);
     this.context = context;
-    this.loadConfig();
+    this.commands = [];
+
+    this.LoadConfig();
   }
 
-  loadConfig() {
-    this.config = vscode.workspace
-      .getConfiguration()
-      .get("jerrychoux.commandOnSave");
+  public LoadConfig() {
+    this.commands = <Array<ICommand>>(
+      vscode.workspace.getConfiguration().get(EXTENSION_NAME)
+    );
+  }
 
-    console.log(">>>config=", this.config);
+  public ExecuteCommands(document: vscode.TextDocument) {
+    if (this.commands.length === 0) {
+      return;
+    }
+
+    let fileExt = document.fileName
+      .substr(document.fileName.lastIndexOf(".") + 1)
+      .toLowerCase();
+    let commands = this.commands.filter(command => fileExt === command.ext);
+
+    if (commands.length === 0) {
+      return;
+    }
+
+    this.output.appendLine("Execute Command for file " + document.fileName);
   }
 }
